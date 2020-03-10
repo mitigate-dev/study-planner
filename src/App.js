4c22714c4fab685a78ace40e96b4d9f47abe3853
Arvis Zeile <arvis.zeile@gmail.com>
2020-03-10 23:13:26 +0200

    Add new block for EBE

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 import IntroText from './IntroText';
 import StudyDirections from './StudyDirections';
 
@@ -26,6 +28,22 @@ function App() {
         <IntroText />
 
         <StudyDirections />
+
+        <Box className="Block-call-to-action">
+          <Button variant="contained" color="primary">
+            Izvēli esmu veicis! Doties tālāk!
+          </Button>
+        </Box>
+
+        <h2 className="Block-title">
+          Otrais solis - iepazīsti individuālo plānu!
+        </h2>
+
+        <p>
+          Individuālais mācību plāns ir teju gatavs! Zaļajos lauciņos vēl jāveic daža kursu izvēles. Apskati savu individuālo plānu. Ja vēlies, nosūti to sev uz e-pastu.
+        </p>
+
+        {/* TODO : TE NĀK EBE BLOKS */}
 
       </Container>
     </div>
