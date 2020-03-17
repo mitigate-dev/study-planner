7144f1696be3f09d2fa8a97ab39b25fb8bbb817d
Edgars Beigarts <edgars.beigarts@gmail.com>
2020-03-17 23:11:42 +0200

    Refactor Directions

--- src/StudyDirections.js
+++ src/StudyDirections.js
@@ -12,7 +12,6 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
-import IntroText from './IntroText';
 
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -97,7 +96,21 @@ function StudyDirections({ onNextStep }) {
         Pirmais solis - izvēlies padziļinātos un specializētos kursus! 
       </h2>
 
-      <IntroText />
+      <Typography variant="body1" paragraph>
+        Katram vidusskolēnam 12. klasē jāapgūst <b>trīs padziļinātie kursi</b>. Padziļinātos kursus skolēns izvēlas, iestājoties vidusskolā. Skolēnam ne vienmēr būs dota iespēja mainīt padziļinātos kursus mācību laikā, tādēļ lēmumiem par padziļināto kursu izvēli jābūt atbildīgiem.
+      </Typography>
+      <Typography variant="body1" paragraph>
+        Svarīgi zināt, ka <b>divos no apgūtajiem</b> padziļinātajiem kursiem skolēnam būs jākārto valsts pārbaudes darbs. Skolēns pats izvēlas, kuros no apgūtajiem padziļinātajiem kursiem kārtot valsts pārbaudes darbus. Valsts pārbaudes darba rezultāti būs nozīmīgi, iestājoties izvēlētajās studiju programmās Latvijā un ārzemēs.
+      </Typography>
+      <Typography variant="body1" paragraph>
+        Ogres 1. vidusskola piedāvās deviņus padziļinātos kursus - Latviešu valoda un literatūra II, Angļu valoda II, Sociālās zinātnes II, Fizika II, Ķīmija II, Bioloģija II, Matemātika II, Dizains un tehnoloģijas II, Programmēšana II.      
+      </Typography>
+      <Typography variant="body1" paragraph>
+        Lai padziļinātos kursus varētu izvēlēties pārdomāti, esam norādījuši, kurus padziļinātos kursus ieteicams izvēlēties sekmīgām studijām dažādos studiju virzienos. Aicinām iedvesmoties no piemēriem! <b>Aicinām izvēlēties un sakārtot prioritātā secībā piecus interesantākos un nākotnei noderīgākos padziļinātos kursus, kurus vēlaties mācīties!</b>
+      </Typography>
+      <Typography variant="body1" paragraph>
+        Līdztekus padziļinātajiem kursiem skolēnam ir iespēja izvēlēties <b>specializētos kursus</b>. Specializēto kursu izvēle un apguve ir brīvprātīga! Ogres 1. vidusskola piedāvās septiņus specializētos kursus - Spāņu valoda, Franču valoda, Filozofija, Uzņēmējdarbības pamati, Projektu vadība, Psiholoģija, Cilvēka bioloģija.  
+      </Typography>
 
       <h2 className="Block-title">
         Studiju virzieni un ieteikumi padziļināto kursu izvēlei
